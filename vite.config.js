dd559f5ca7548fbc06fc65f78502903539348897
pntruong17 <pntruong17@gmail.com>
2024-11-20 17:52:31 +0700

    update1.2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
+  build: {
+    outDir: 'dist' // Thư mục đầu ra sau khi build
+  }
 })
